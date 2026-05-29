beef6bf3fc31fe50f8089b26bf1e30f3ead62460
pls3 <1529197501@qq.com>
2026-05-29 20:31:47 +0800

    把类的小练习写完了，不错，继续努力！

--- 类/小练习.py
+++ 类/小练习.py
@@ -25,6 +25,35 @@ class manager:
         else:
             print(f'没有找到学号为{sid}的学生')
 
+    def show_all(self):
+        if self.stu_list:
+            for stu in self.stu_list:
+                # Student类的__str__方法已经定义，这里直接调用即可输出学生信息
+                print(stu)
+        else:
+            print('当前没有学生信息')
+
+    def set_score(self):
+        sid=input('请输入要设置成绩的学生学号：')
+        target=None
+        for stu in self.stu_list:
+            if stu.stu_id==sid:
+                target=stu
+                break
+        if target:
+            socre_str=input('请输入成绩（学科-分数，学科-分数）：')
+            score_list=socre_str.replace('，',',').split(',')
+            for item in score_list:
+                course,score=item.split('-')
+                course=course.strip()
+                score=score.strip()
+                target.add_score(course,float(score))
+                print(f'设置成功，{target.name}的{course}的成绩为{score}')
+            print(f'设置成功，{target.name}的平均分为{target.calcu_avg()}')
+        else:
+            print(f'没有找到学号为{sid}的学生')
+
+
     def display(self) -> str:
         menu = []
         menu.append(f"--- 学生管理系统 ---")
@@ -37,6 +66,23 @@ class manager:
     
         return "\n".join(menu)
 
+    def run(self):
+        while True:
+            print(self.display())
+            choice=input('请输入操作序号：')
+            if choice=='1':
+                self.add_student()
+            elif choice=='2':
+                self.delete_student()
+            elif choice=='3':
+                self.show_all()
+            elif choice=='4':
+                self.set_score()
+            elif choice=='5':
+                print('谢谢使用')
+                break
+            else:
+                print('输入错误，请重新输入')
 
 
 class Person:
@@ -46,24 +92,18 @@ class Person:
         self.gender=gender
 
 class Student(Person):
-    
     count=0
-
     def __init__(self,name,age,gender) -> None:
         super().__init__(name,age,gender)
         Student.count += 1
         self.stu_id=f'{datetime.now().year}{Student.count:03d}'
         self.scores={}
-
     def __str__(self) -> str:
         return f'''学生姓名：{self.name}，年龄：{self.age}，性别：{self.gender}
 学号：{self.stu_id}，{self.calcu_avg()}'''
-
     #add方法
     def add_score(self,course,score):
         self.scores[course]=score
-
-
     # 计算平均分
     def calcu_avg(self):
         if self.scores:
@@ -73,14 +113,5 @@ class Student(Person):
 
 
 
-
-s1=Student('张三',18,'男')
-s1.add_score('语文',85)
-s1.add_score('数学',90)
-s1.add_score('英语',88)
-print(s1.calcu_avg())
-s2=Student('李四',19,'女')
-print(s2.calcu_avg())
-
-
-
+m=manager()
+m.run()
